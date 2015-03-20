b5639fa7a062216d1c6c4b253b1ab5043bc938cd
Jenni Nurmi <jenniknurmi@gmail.com>
2015-03-20 19:05:59 +0200

    pitraportti toimii

---
+++ Pyrre/src/test/java/pyrre/pyrre/kortit/KorttiTest.java
@@ -0,0 +1,61 @@
+
+package pyrre.pyrre.kortit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import pyrre.pyrre.kortit.Kortti;
+
+/**
+ *
+ * @author Jenni
+ */
+public class KorttiTest {
+
+    private Kortti kortti;
+
+    public KorttiTest() {
+
+    }
+    
+    @Before
+    public void setUp() {
+        kortti = new Kortti(10,3);
+    }
+
+    @Test
+    public void KorttiToStringToimii() {
+        assertEquals("  Pata 10", kortti.toString());
+    }
+    
+    @Test
+    public void KorttiGetArvoToimii() {
+        assertEquals(10, kortti.getArvo());
+    }
+    
+    @Test
+    public void KorttiGetMaaToimii() {
+        assertEquals(3, kortti.getMaa());
+    }
+    
+    @Test
+    public void KorttiGetPaikkaPalauttaaOletuksenaPakka(){
+        assertEquals("pakka", kortti.getPaikka());
+    }
+    
+    @Test
+    public void asetaUusiPaikkaToimii() {
+        kortti.asetaUusiPaikka("poyta");
+        assertEquals("poyta", kortti.getPaikka());
+    }
+    
+    @Test
+    public void luoTyhjaKorttiToimii() {
+        Kortti tyhja = kortti.LuoTyhjaKortti();
+        assertEquals("        ", tyhja.toString());
+    }
+
+}
